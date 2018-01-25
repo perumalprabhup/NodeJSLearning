7ae3a2d6fa87507bd16c4b5d134f38ac54e58b8f
perumalprabhup <ppprabhu2010@gmail.com>
2018-01-25 17:59:00 +0530

    callbacks

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ var rect = require('./rectangle');
 
 function solveRect(l,b){
 console.log("Solving for rectangle with l= " + l + " and b " + b);
-if(l<=0 || b<=0){
-	console.log("Rec dimentions should be greater than zero: l "+l +" and b "+b);
-}
+rect(l,b,(err,rect)=>{
+ if (err) {
+	        console.log("ERROR: ", err.message);
+	    }
 else{
-	console.log("area of the Rect with l = " + l +" and b = "+ b + " area = "+rect.area(l,b));
-	console.log("perimeter of the Rect with l = " + l +" and b = "+ b + " Perimeter = "+rect.perimeter(l,b));	
+	console.log("area of the Rect with l = " + l +" and b = "+ b + " area = "+rect.area());
+	console.log("perimeter of the Rect with l = " + l +" and b = "+ b + " Perimeter = "+rect.perimeter());	
 }
-}
+});
+ console.log("This statement after the call to rect()");
+};
+
 
 solveRect(10,15);
